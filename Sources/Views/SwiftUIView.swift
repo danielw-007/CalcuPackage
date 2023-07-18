a414c2ec0d9e0a865471208d589e480ddb0e0ad6
danielw-007 <daniel030@binus.ac.id>
2023-07-18 15:33:30 +0700

    UPDATE

--- Sources/Views/SwiftUIView.swift
+++ Sources/Views/SwiftUIView.swift
@@ -12,6 +12,7 @@ public struct SwiftUIView: View {
     @available(iOS 13.0, *)
     public var body: some View {
         Text("This is from package manager!")
+        Text("UPDATE!!!")
     }
 }
 
